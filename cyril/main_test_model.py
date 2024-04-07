8973cf67b7e6f7308f2170cf433c44cc2cfe157e
@cnebanga <cnebanga@gmail.com>
2024-04-07 14:27:27 +0200

    model à 10000ep

--- cyril/main_test_model.py
+++ cyril/main_test_model.py
@@ -20,7 +20,7 @@ GameTime = 0
 GameHistory = []
 renderFlag = True
 
-ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=5, epsilon=0.02, epsilon_end=0.01, epsilon_dec=0.999, replace_target= REPLACE_TARGET, batch_size=64, input_dims=19,fname='test.h5')
+ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=5, epsilon=0.02, epsilon_end=0.01, epsilon_dec=0.999, batch_size=512, replace_target= REPLACE_TARGET, input_dims=19, fname='ddqn_model.h5')
 
 ddqn_agent.load_model()
 ddqn_agent.update_network_parameters()
